9e3fd5f3ffae42d13936c7ec768e06115545af23
OscarThomason <83683164+OscarThomason@users.noreply.github.com>
2026-01-28 14:33:38 -0600

    Use dynamic PORT

--- src/server.js
+++ src/server.js
@@ -19,6 +19,8 @@ app.get("/products", async (req, res) => {
   res.json(products);
 });
 
-app.listen(3000, () => {
-  console.log("🚀 API corriendo en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 API corriendo en puerto ${PORT}`);
 });
